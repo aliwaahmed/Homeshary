433e5fe08ef599ab2fa0663c548fd6758af32c9b
aliwaahmed <aliwaahmed/iChat>
2020-07-19 14:12:02 +0200

    init

---
+++ app/src/main/java/com/customer/shary/live/ui/settings/terms.java
@@ -0,0 +1,16 @@
+package com.customer.shary.live.ui.settings;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.customer.shary.live.R;
+
+public class terms extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.terms_layout);
+    }
+}
